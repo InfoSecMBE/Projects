9b62d81037afef2b85f82982b69ed1ae3cf10061
InfoSecMBE <infosecmbe@gmail.com>
2024-11-24 16:36:40 +0000

    Cleaning up first script
    Saving first script

---
+++ Powershell/Workstation_NIST_Powershell.ps1
@@ -0,0 +1,3 @@
+# Author Michael Ellis
+# This powershell project script will use Wazuh as the source for issues identified under secure configuration assesment. These script will attempt to remediate those findings.
+$PSVersionTable
