c3cc4f7ee565d62e276eef4687ad6f654821d09e
InfoSecMBE <infosecmbe@gmail.com>
2024-11-27 16:11:26 +0000

    Update Workstation_NIST_Powershell.ps1
    -Enforce password history
    -Minimum password age

--- Powershell/Workstation_NIST_Powershell.ps1
+++ Powershell/Workstation_NIST_Powershell.ps1
@@ -1,4 +1,9 @@
 # Author Michael Ellis
 #CIS Microsoft Windows 11 Enterprise Benchmark v1.0.0
 # This powershell project script will use Wazuh as the source for issues identified under secure configuration assesment. These script will attempt to remediate those findings.
-$PSVersionTable
+
+#Step 1 - Enforce password history - more than 24
+net accounts /uniquepw:24
+
+#Step 2 - Change minimum password age - more than 1 day
+net accounts /minpwage:1
