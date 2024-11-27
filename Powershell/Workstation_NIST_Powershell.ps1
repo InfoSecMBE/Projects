c2f91cafa3ced1cc9e04064a3c5bf924a1ff6f4e
InfoSecMBE <infosecmbe@gmail.com>
2024-11-27 11:27:00 +0000

    Update Workstation_NIST_Powershell.ps1
    new description

--- Powershell/Workstation_NIST_Powershell.ps1
+++ Powershell/Workstation_NIST_Powershell.ps1
@@ -1,3 +1,4 @@
 # Author Michael Ellis
+#CIS Microsoft Windows 11 Enterprise Benchmark v1.0.0
 # This powershell project script will use Wazuh as the source for issues identified under secure configuration assesment. These script will attempt to remediate those findings.
 $PSVersionTable
