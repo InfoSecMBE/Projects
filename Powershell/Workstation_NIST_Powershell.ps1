c8ab50b767c122a65d998d6d541e96deca529b68
InfoSecMBE <infosecmbe@gmail.com>
2024-11-28 14:18:11 +0000

    Update Workstation_NIST_Powershell.ps1
    -account lockout window
    -Rename local guest
    -Rename local administrator
    -Relax minimum password length
    -Block Microsoft accounts
    -Eject removable NTFS media

--- Powershell/Workstation_NIST_Powershell.ps1
+++ Powershell/Workstation_NIST_Powershell.ps1
@@ -16,3 +16,22 @@ net accounts /lockoutduration:15
 
 # Step 5 - Change account lockout threshold - less than 5 - Command Prompt
 net accounts /lockoutthreshold:5
+
+# Step 6 - Reset account lockout window - More than 15 - Command Prompt
+net accounts /lockoutwindow:15
+
+# Step 7 - Rename local guest account - change value - PowerShell
+Rename-LocalUser -Name Guest -NewName localgst
+
+# Step 8 - Rename local administrator account - change value - PowerShell
+Rename-LocalUser -Name Administrator -NewName localadmin
+
+# Step 9 - Relax minimum password length limits - enabled - PowerShell
+REG ADD HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SAM /v RelaxMinimumPasswordLengthLimits /t REG_DWORD /d 1 /f
+
+# Step 10 -  Block Microsoft accounts - PowerShell
+REG ADD HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System /v NoConnectedUser /t REG_DWORD /d 3 /f
+
+# Step 11 - Eject removable NTFS media - PowerShell
+REG ADD "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateDASD /t REG_DWORD /d 2 /f
+
